0c411f36de567d0e49e7e86c19a4c3d44d6b731f
Abelmf98 <91120898+Abelmf98@users.noreply.github.com>
2022-04-18 22:15:41 +0200

    algunos ejercicios en c

---
+++ array/9.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#define TOTAL 10
+
+	/*
+		Hacer un programa para entrar diez números en una tabla y que después los sume. No sumarlos a
+		medida que se van entrando. Hacer dos bucles, un para entrar los elementos, y otro para sumarlos.
+	*/
+	
+	//Variables Globales
+		int t[TOTAL], i, s=0;
+
+main(){
+	
+	
+	//Introducir tabla
+	for (i=0; i<TOTAL; i++) {
+		
+		printf("Introduce el numero %d: ", i);
+		scanf("%d", &t[i]);			
+	}
+	
+	//Sumarlos
+	for (i=0; i<TOTAL; i++) {
+		
+		s+=t[i];
+	}
+	
+	//Visualizar la suma
+	printf("\nEl total de la suma es: %d", s);
+	
+	return 0;
+}
