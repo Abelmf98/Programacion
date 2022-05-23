d257e65ba9125fcd5db6b9f88af393db5b60116b
Abelmf98 <91120898+Abelmf98@users.noreply.github.com>
2022-05-24 00:40:02 +0200

    Palindromo

---
+++ cadena_caracteres/palindromo.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+char cadena[50];
+int longitud,final,i;
+
+main()
+{
+	printf("PALABRAS PALINDROMAS\n");
+	printf("\nEscribe una palabra palindroma: ");
+	scanf(" %s",cadena);
+	
+	longitud=strlen(cadena);
+	longitud=longitud-1;
+	
+	for(i=0,final=longitud;i<=longitud/2;i++,final--){
+		if(cadena[i]==cadena[final]){ 
+		     printf("Es palindromo");
+		}else{
+	        printf("No es palindromo");
+	    }
+	    break;
+	}
+	return 0;
+}
